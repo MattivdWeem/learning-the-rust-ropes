cd06113ce54bbf1402d126967c79a6b0eeb44eed
Matti van de Weem <mvdweem@gmail.com>
2017-11-17 23:02:32 -0500

    the game works

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,8 +1,17 @@
+extern crate rand;// use the external rand lib
 use std::io; // use the io library from the standards library
+use std::cmp::Ordering; // from standards, get compare
+use rand::Rng; // from the rand lib, use the Range lib
 
 fn main() {
     // print a simple line
     println!("Guess the number!");
+
+    // create the random number between 1 and 101 using the rand range lib
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    // print the secret number
+    println!("The secret number is: {}", secret_number);
 
     // print a second line to request input
     println!("Please input your guess.");
@@ -18,5 +27,20 @@ fn main() {
 
     // return your guess in a variable bound print line
     println!("You guessed: {}", guess);
+
+    // ccheck if the input is a string or an number
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
+    // match one of the multiple options with the value, use the compar library
+    match guess.cmp(&secret_number) {
+        Ordering::Less    => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal   => println!("You win!"),
+    }
+
+
+
+
 
 }
