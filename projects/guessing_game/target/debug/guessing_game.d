a55a95cdf29bca0f276abae0ccd9f2192acf72b0
Matti van de Weem <mvdweem@gmail.com>
2017-11-17 22:35:46 -0500

    fix

--- projects/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/mattivandeweem/Development/hobby/rust/hello_cargo/projects/guessing_game/target/debug/guessing_game: /Users/mattivandeweem/Development/hobby/rust/hello_cargo/projects/guessing_game/src/main.rs
