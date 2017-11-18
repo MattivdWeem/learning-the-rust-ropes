1135ce18ab919febc5bc39d209c80b442b9c8f20
Matti van de Weem <mvdweem@gmail.com>
2017-11-18 13:07:10 -0500

    borrowing and referencing

--- projects/branches/src/main.rs
+++ projects/branches/src/main.rs
@@ -5,6 +5,29 @@ fn main() {
     println!("condition was {}", condition);
     println!("The value of number is: {}", numb);
 
+
+    let a = [10, 20, 30, 40, 50];
+
+    // iterate over the elements in an array
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    // do the same thing, with a generated list lengt, reversed
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    // test string
+    let mut s:String = String::from("hello");
+    takes_ownership(&mut s);
+    println!("{}",s);
+
+}
+
+fn takes_ownership(s: &mut String){
+    s.push_str(", world");
 }
 
 // revert a boolean
